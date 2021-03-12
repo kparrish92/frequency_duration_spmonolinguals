4e01e38ee66019ae846bf347f2c68bb43a8ab7e6
Juan Jose Garrido <jjg280@rutgers.edu>
2021-03-11 19:28:06 -0500

    added readme files and libs script

---
+++ scripts/00_libraries.R
@@ -0,0 +1,23 @@
+# load libraries ---------------------------
+
+library("tidyverse")
+library("papaja")
+library("knitr")
+library("here")
+library("fs")
+library("purrr")
+library("dplyr")
+library("tidyr")
+library("forcats")
+library("readr")
+library("readxl")
+library("writexl")
+library("glue")
+library("ggplot2")
+library("ganttrify")
+library("contributoR")
+library("magick")
+library("webshot")
+library("kableExtra")
+
+# ------------------------------------------
